b782c58796d468ac86f2a0ce42dcf2e0de2784e9
Alexander Zhuykov <a.zhuykov.98@mail.ru>
2022-10-11 09:26:09 +0000

    Resolve "Разработать api которое считает количество вопросов в базе"

--- src/main/java/com/javamentor/qa/platform/dao/impl/model/QuestionDaoImpl.java
+++ src/main/java/com/javamentor/qa/platform/dao/impl/model/QuestionDaoImpl.java
@@ -2,11 +2,13 @@ package com.javamentor.qa.platform.dao.impl.model;
 
 import com.javamentor.qa.platform.dao.abstracts.model.QuestionDao;
 import com.javamentor.qa.platform.dao.impl.repository.ReadWriteDaoImpl;
+import com.javamentor.qa.platform.dao.util.SingleResultUtil;
 import com.javamentor.qa.platform.models.entity.question.Question;
 import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import java.util.Optional;
 
 @Repository
 public class QuestionDaoImpl extends ReadWriteDaoImpl<Question, Long> implements QuestionDao {
@@ -14,4 +16,12 @@ public class QuestionDaoImpl extends ReadWriteDaoImpl<Question, Long> implements
     @PersistenceContext
     private EntityManager entityManager;
 
+    @Override
+    public Optional<Long> getQuestionCount() {
+        return SingleResultUtil.getSingleResultOrNull(entityManager.createQuery("""
+                    SELECT COUNT(q)
+                    FROM Question q
+                    WHERE q.isDeleted = false
+                """));
+    }
 }
